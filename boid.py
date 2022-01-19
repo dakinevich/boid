b4385d6ff41731bc32b58434d121f3b2e19563b3
dakinevich <57320289+dakinevich@users.noreply.github.com>
2022-01-20 00:20:21 +0300

    better boids

--- boid.py
+++ boid.py
@@ -16,31 +16,33 @@ class Bird():
     def __init__(self, pos=[0, 0], vlc=[0, 0], nose_color=(0, 0, 255)):
         self.pos = pos
         self.vlc = vlc
-        self.cruis_vlc = 5  # pixels per second
+        self.cruis_vlc = 8  # pixels per second
         self.nose_color = nose_color
 
     def frame(self, birds, delta_t, w, h):
         forse_out = 0.5  # forse that repels
         forse_in = 0.2
-        birds = sorted(birds, key=lambda bird: get_dist(
-            bird.pos, self.pos))
+        back_forse_range = 30
+        to_forse_range = 40
 
+        birds = [[bird, get_dist(bird.pos, self.pos)] for bird in birds]
+        birds = sorted(birds, key=lambda bird_tuple: bird_tuple[1])
         bird_close = birds[0]
-        d = get_dist(self.pos, bird_close.pos) + 0.00001  # != 0
-        if d < 40:
+        d = bird_close[1] + 0.0000001  # != 0
+        if d < back_forse_range:
             self.vlc = [
-                self.vlc[i] - forse_out*(bird_close.pos[i] -
+                self.vlc[i] - forse_out*(bird_close[0].pos[i] -
                                          self.pos[i])/d for i in [0, 1]]
-
-        bird_far = birds[-1]
-        d = get_dist(self.pos, bird_far.pos) + 0.00001  # != 0
-        if d > 60:
+        birds = [bird for bird in birds if bird[1] > to_forse_range]
+        bird_far = birds[0]
+        d = bird_far[1] + 0.0000001  # != 0
+        if d > to_forse_range:
             self.vlc = [
-                self.vlc[i] + forse_in*(bird_far.pos[i] -
+                self.vlc[i] + forse_in*(bird_far[0].pos[i] -
                                         self.pos[i])/d for i in [0, 1]]
 
         n = 1  # 1/speed stabilisator strengh
-        vlc_module = hypo(*self.vlc)
+        vlc_module = hypo(*self.vlc) + 0.0000001  # != 0
         cruis_hff = self.cruis_vlc/vlc_module
         new_kff = 1 + (cruis_hff - 1)*(n*delta_t)
         self.vlc = [vlc*new_kff for vlc in self.vlc]
@@ -103,10 +105,10 @@ while running:
             if event.key == pyg.K_ESCAPE:
                 running = False
 
-    # window.fill((0, 0, 0))
-    s = pyg.Surface((w, h), pyg.SRCALPHA)
-    s.fill((0, 0, 0, 32))
-    window.blit(s, (0, 0))
+    window.fill((0, 0, 0))
+    # s = pyg.Surface((w, h), pyg.SRCALPHA)
+    # s.fill((0, 0, 0, 32))
+    # window.blit(s, (0, 0))
 
     delta_t = 0.1  # time.time()-t
     prosess_birds(birds, delta_t)
